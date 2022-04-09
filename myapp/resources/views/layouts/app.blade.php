d003bfe63db411a98074ee9889ae485b2309646f
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-04-09 23:52:04 +0300

    add

---
+++ myapp/resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.1/dist/flowbite.min.css" />
+    @livewireStyles
+</head>
+<body>
+    
+    <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+    @livewireScripts
+</body>
+</html>
