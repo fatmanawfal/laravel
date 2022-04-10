238e0adfe45c0f910d78776bbd180f43caf23440
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-04-10 16:10:54 +0300

    add

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Cars;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+//car page
+Route::get('/car', Cars::class);
