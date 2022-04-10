44cf1aa84c51b272d012bb95cf91c8b00e462124
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-04-10 15:00:08 +0300

    Update 2022_04_09_212741_create_cars_table.php

--- myapp/database/migrations/2022_04_09_212741_create_cars_table.php
+++ myapp/database/migrations/2022_04_09_212741_create_cars_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('type');
+            $table->string('color');
+            $table->string('number');
             $table->timestamps();
         });
     }
